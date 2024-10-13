c6d2beff1e783b4f9c3c6fc1d8ba416cc2ce0447
David <xmontanoga@ucvvirtual.edu.pe>
2024-10-13 00:04:13 -0500

    hero create

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,7 +8,11 @@ export default {
 		"./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
 	],
 	theme: {
-		extend: {},
+		extend: {
+			backgroundImage: {
+				'custom-gradient': 'radial-gradient(circle 550px at 65% 0%, var(--tw-gradient-stops))',
+			  },
+		},
 	},
 	plugins: [nextui(), animations]
 }
