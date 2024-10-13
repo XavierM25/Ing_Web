517c316e4b8d1a37d1c6abfdf90ffceb6cb50fdd
zentoo31 <zentoo31@gmail.com>
2024-10-12 23:59:22 -0500

    updatew

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,12 @@ import node from "@astrojs/node";
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
+import auth from 'auth-astro';
+
 // https://astro.build/config
 export default defineConfig({
   output:'server',
   adapter: node({mode:'standalone'}),
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), auth()],
 
 });
---
+++ src/components/app.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Routes, Route, Outlet, Link } from 'react-router-dom'
+
+export default function App() {
+  return (
+    <div>app</div>
+  )
+}
